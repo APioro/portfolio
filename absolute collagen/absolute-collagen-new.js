c8cf9a36b29adce26bdcf6bb46c781eb46098a9c
APioro <pioroangelika@gmail.com>
2025-11-07 14:10:55 +0100

    ac text update

--- absolute collagen/absolute-collagen-new.js
+++ absolute collagen/absolute-collagen-new.js
@@ -15,13 +15,13 @@ const projects = [
 
     {
         type: "text",
-        subtitle: "design",
-        description: "description1",
+        subtitle: "Identifying Core Problems",
+        description: "I given full freedom to decide the project’s focus, so I began by evaluating the existing webshop using heuristic analysis and quick user feedback sessions. The most critical issues emerged around navigation and product discovery, clarity of product information and purchase model, and overall brand coherence. These became the main areas I prioritized for redesign.",
     },
     {
         type: "text",
-        subtitle: "my design",
-        description: "description1",
+        subtitle: "navigation",
+        description: "The original mobile navigation felt cluttered and inconsistent, mixing articles, blog posts, and products under unclear labels. I reduced the number of categories to four to create a more focused structure with fewer distractions. This helped users quickly understand where to go and made the browsing experience feel calmer and more intentional.",
     },
 
 
@@ -40,13 +40,13 @@ const projects = [
 
         {
         type: "text",
-        subtitle: "design",
-        description: "description1",
+        subtitle: "Product showcase & pricing",
+        description: "I added lifestyle previews to show the product in action and encourage clicks. A clickable rating next to the title directs users to reviews faster. I clarified pricing by showing cost per sachet, and made the total order cost, any gift offers, and delivery details very prominent — reducing surprises and building trust.",
     },
     {
         type: "text",
-        subtitle: "my design",
-        description: "description1",
+        subtitle: "Information Hierarchy",
+        description: "The challenge was that Absolute Collagen’s site had a wealth of content, but it was overwhelming and hard to navigate. I restructured the product information based on a user card-sorting exercise, categorizing content where users expected to find it. I added bullet points for quick scanning and combined ingredient details with usage instructions, addressing a key need for clarity. I also created product highlights and infographics to showcase the supplement’s benefits and results, establishing a clear hierarchy and making the page both informative and visually digestible.",
     },
 
 
@@ -66,13 +66,13 @@ const projects = [
 
         {
         type: "text",
-        subtitle: "design",
-        description: "description1",
+        subtitle: "Brand consistency",
+        description: "I noticed the brand had multiple visual directions across the site, so I streamlined it by choosing a single primary yellow, unifying buttons, icons, and typography, and designing flexible assets for infographics and social media. The goal was a clean, modern look with a luxurious feel, making the site consistent, engaging, and accessible for the audience.",
     },
     {
         type: "text",
-        subtitle: "my design",
-        description: "description1",
+        subtitle: "...AND DONE!",
+        description: "Refining an existing, content-rich brand—improving clarity, consistency, and engagement—can be harder than starting from scratch, but it’s also incredibly rewarding. I approached it strategically. There was a huge amount of invisible work in this project, from UX standards alignment and competitor analysis to technical considerations.",
     },
 
 ];
